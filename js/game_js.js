d54193791370592c38a796a3bdad32946f459933
Craig <cgleckman@gmail.com>
2015-06-23 11:31:43 -0700

    removed unused file

--- js/game_js.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".container").css("width", $(window).width());
-	$(".container").css("height", $(window).height());
-	$(".dice").height($(".dice").width());
-	$("#roll_button").height($(".dice").width());
-	
-});
